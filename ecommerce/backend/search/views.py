44e052a525e283eb2bcaf3491035d3273804aa71
tejash <tejashyadavinfo@gmail.com>
2025-08-26 15:31:49 +0530

    learning django rest framework

---
+++ ecommerce/backend/search/views.py
@@ -0,0 +1,37 @@
+from . import client
+from rest_framework import generics
+from products.models import Product
+from rest_framework.response import Response
+from products.serializers import ProductSerializer
+
+class SearchListView(generics.GenericAPIView):
+    def get(self, request, *args, **kwargs):
+        user = None
+        if request.user.is_authenticated:
+            user = request.user.username
+        query = request.GET.get('q')
+        public = str(request.GET.get('public')) != "0"
+        tag = request.GET.get('tag') or None
+        if not query:
+            return Response({"message": "Query not found"})
+        results = client.perform_search(query, tags=tag, user=user, public=public)
+        return Response(results)
+
+search_list_view = SearchListView.as_view()
+
+class SearchListOldView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        q = self.request.GET.get('q')
+        results = Product.objects.none()
+        if q is not None:
+            user = None
+            if self.request.user.is_authenticated:
+                user = self.request.user
+            results = qs.search(q, user=user)
+        return results
+
+search_list_old_view = SearchListOldView.as_view()
